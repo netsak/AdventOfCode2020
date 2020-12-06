c62d002f38de430b0c002fda82417f13056e21bb
Sir Rob <git@netsak.de>
2020-12-06 11:36:04 +0100

    Solve day 6 part 2

--- day-06.py
+++ day-06.py
@@ -1,28 +1,44 @@
 #!/usr/bin/env python
 from functools import reduce
-from pprint import pprint
+from operator import add, and_
 
 
 def get_group_questions(filename):
-    """Load the questions for each group"""
+    """Load all answered questions for each group"""
     with open(filename) as f:
         content = f.read().split("\n\n")
         group_questions = [set(c for c in line if c != "\n") for line in content]
         return group_questions
 
 
-def sum_questions(questions):
-    count = len(tuple(y for q in questions for y in q))
+def sum_answers(questions):
+    count = reduce(add, (len(answer) for question in questions for answer in question))
     print(f"The sum of all answered questions is {count}")
     return count
 
 
+def get_common_group_questions(filename):
+    """Load all answered questions for each group answered by each group member"""
+    with open(filename) as f:
+        groups = f.read().split("\n\n")
+        split_set = lambda group: (set(group) for group in group.split("\n"))
+        group_sets = (map(split_set, groups))
+        group_questions = (reduce(and_, group) for group in group_sets)
+        return group_questions
+
+
 if __name__ == "__main__":
-    # example
+    print("Example")
     questions = get_group_questions("data/day-06-example1.txt")
-    count = sum_questions(questions)
+    count = sum_answers(questions)
     assert count == 11
-    # reality
+    questions = get_common_group_questions("data/day-06-example1.txt")
+    count = sum_answers(questions)
+    assert count == 6
+    print("Reality")
     questions = get_group_questions("data/day-06.txt")
-    count = sum_questions(questions)
+    count = sum_answers(questions)
     assert count == 6297
+    questions = get_common_group_questions("data/day-06.txt")
+    count = sum_answers(questions)
+    assert count == 3158
