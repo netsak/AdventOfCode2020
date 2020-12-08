c24834df06856276187c3ff2364848a3de671fbd
Sir Rob <git@netsak.de>
2020-12-08 12:21:24 +0100

    Solve day 8 part2

--- day-08.py
+++ day-08.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
-from functools import reduce
-from operator import add, and_
 from dataclasses import dataclass
-from typing import List
-import re
-from pprint import pprint
-
-regex_content = re.compile(r"(\d+) ((\w+\s?)+) bag")
-regex_bag = re.compile(r"(.*?) bags contain")
+from copy import deepcopy
 
 
 @dataclass
@@ -30,8 +23,10 @@ def load_program(filename):
         program = [parse_command(line) for line in f.readlines()]
         return program
 
+
 def run(program):
     """Runs the program and prints the accumulator if an error occurs"""
+    program = deepcopy(program)
     accumulator = 0
     try:
         length = len(program)
@@ -47,17 +42,38 @@ def run(program):
             else:
                 i += 1
     except Exception as ex:
-        print(ex)
-        print(f"accumulator before error: {accumulator}")
-    return accumulator
+        print(f"ERROR: {ex}: accumulator before error: {accumulator}")
+        return accumulator, False
+    return accumulator, True
+
+
+def fix_it(program):
+    """Tries to fix the program by changing switching nop and jmp statements"""
+    for i, cmd in enumerate(program):
+        current_program = deepcopy(program)
+        if cmd.cmd == "jmp":
+            current_program[i].cmd = "nop"
+        elif cmd.cmd == "nop":
+            current_program[i].cmd = "jmp"
+        else:
+            continue
+        accumulator, success = run(current_program)
+        if success:
+            print(f"accumulator after successful run: {accumulator}")
+            return accumulator
 
 
 if __name__ == "__main__":
     print("Example")
     program = load_program("data/day-08-example1.txt")
-    accumulator = run(program)
-    assert accumulator == 5
+    result = run(program)
+    assert result == (5, False)
+    accumulator = fix_it(program)
+    assert accumulator == 8
     print("Reality")
     program = load_program("data/day-08.txt")
-    accumulator = run(program)
-    # assert accumulator == 5
+    result = run(program)
+    assert result == (1675, False)
+    accumulator = fix_it(program)
+    assert accumulator == 1532
+
