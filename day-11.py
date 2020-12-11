9d921e6b5dc7723921790ca5e0a3bd85559e9bce
Sir Rob <git@netsak.de>
2020-12-11 12:31:15 +0100

    Solve day 11

--- day-11.py
+++ day-11.py
@@ -1,79 +1,94 @@
 #!/usr/bin/env python
-from collections import defaultdict
-from itertools import accumulate
-from functools import reduce, partial
-import timeit
+from copy import deepcopy
+from time import sleep
 
-def load_adapters(filename):
+
+slopes = ( (-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (1, -1), (1, 1), (-1, -1) )
+
+
+def load_seats(filename):
     with open(filename) as f:
-        adapters = [int(line) for line in f.readlines()]
-        return adapters
+        seats = ["."+line.strip()+"." for line in f.readlines()]
+        seats.insert(0, "." * len(seats[0]))
+        seats.append("." * len(seats[0]))
+        seats = [list(row) for row in seats]
+        return seats
 
 
-def find_differences_and_product(adapters,  max_diff=3, build_in_joltage=3):
-    adapters = sorted([0] + adapters)
-    adapters.append(adapters[-1] + build_in_joltage)
-    differences = tuple((b - a for a,b in zip(adapters,adapters[1:])))
-    assert not any(d for d in differences if d > max_diff), f"difference must not be greater than {max_diff}"
-    diff_1 = sum(1 for d in differences if d == 1)
-    diff_3 = sum(1 for d in differences if d == 3)
-    product = diff_1 * diff_3
-    print(f"There are {diff_1} differences of 1 jolt and {diff_3} differences of 3 jolt.", end=" ")
-    print(f"The product joltage is {product}.")
-    return diff_1,diff_3, product
+def simple_print(seats):
+    for row in seats:
+        print("".join(row))
+    print()
 
 
-def find_differences_and_product2(adapters, build_in_joltage=3):
-    differences = defaultdict(int)
-    differences[build_in_joltage] = 1
-    adapters = sorted(adapters)
-    last = 0
-    for adapter in adapters:
-        differences[adapter-last] += 1
-        last = adapter
-    joltage_product = differences[1] * differences[3]
-    print(f"There are {differences[1]} differences of 1 jolt and {differences[3]} differences of 3 jolt.", end=" ")
-    print(f"The product joltage is {joltage_product}.")
-    return differences[1], differences[3], joltage_product
-                
+def direct_neighbors(seats, x, y):
+    kernel = seats[y-1][x-1:x+2]+[seats[y][x-1]]+[seats[y][x+1]]+seats[y+1][x-1:x+2]
+    count = sum(1 for k in kernel if k == "#")
+    return count
 
-def find_combinations(adapters, max_diff=3):
-    paths = defaultdict(int)
-    paths[0] = 1
-    adapters = sorted(adapters)
-    for joltage in adapters:
-        paths[joltage] = sum(paths[j] for j in range(joltage-max_diff, joltage))
-    combinations = paths[adapters[-1]]
-    print(f"Found {combinations} disctinct arrangements.")
-    return combinations
+
+def line(x0, y0, dx, dy, max_x, max_y):
+    x = x0
+    y = y0
+    while x < max_x and y < max_y and x > 0 and y > 0:
+        y += dy
+        x += dx
+        yield x, y
+
+
+def line_of_sight(seats, sx, sy):
+    max_y = len(seats) - 1
+    max_x = len(seats[0]) - 1
+    count = 0
+    for dx, dy in slopes:
+        for x, y in line(sx, sy, dx, dy, max_x, max_y):
+            seat = seats[y][x]
+            if seat == "#":
+                count += 1
+                break
+            elif seat == "L":
+                break
+    return count
+
+
+def evolve(seats, max_occupied, kernel):
+    new_seats = deepcopy(seats)
+    max_y = len(seats)
+    max_x = len(seats[0])
+    for y in range(1, max_y-1):
+        for x in range(1, max_x-1):
+            seat = seats[y][x]
+            count = kernel(seats, x, y)
+            if seat == "L" and count == 0:
+                new_seats[y][x] = "#"
+            elif seat == "#" and count >= max_occupied:
+                new_seats[y][x] = "L"
+    simple_print(new_seats)
+    return new_seats
+
+
+def find_stable_configuration(seats, max_occupied, kernel, delay=0):
+    old_seats = None
+    while seats != old_seats:
+        old_seats = seats
+        seats = evolve(seats, max_occupied, kernel)
+        sleep(delay)
+    occupied = sum(1 for row in seats for s in row if s == "#")
+    print(f"{occupied} seats are occupied")
+    return occupied
 
 
 if __name__ == "__main__":
     print("Example")
-    adapters = load_adapters("data/day-10-example1.txt")
-    result = find_differences_and_product(adapters)
-    assert result == (7, 5, 35)
-    # print("timeit 1", timeit.repeat(partial(find_differences_and_product, adapters)))
-    result = find_differences_and_product2(adapters)
-    assert result == (7, 5, 35)
-    # print("timeit 2", timeit.repeat(partial(find_differences_and_product2, adapters)))
-    result = find_combinations(adapters)
-    assert result == 8
-    adapters = load_adapters("data/day-10-example2.txt")
-    result = find_differences_and_product(adapters)
-    assert result == (22, 10, 220)
-    result = find_differences_and_product2(adapters)
-    assert result == (22, 10, 220)
-    result = find_combinations(adapters)
-    assert result == 19208
+    seats = load_seats("data/day-11-example1.txt")
+    simple_print(seats)
+    result = find_stable_configuration(seats, 4, direct_neighbors)
+    assert result == 37
+    result = find_stable_configuration(seats, 5, line_of_sight)
+    assert result == 26
     print("Reality")
-    adapters = load_adapters("data/day-10.txt")
-    result = find_differences_and_product(adapters)
-    assert result == (69, 33, 2277)
-    # print("timeit 1", timeit.repeat(partial(find_differences_and_product, adapters)))
-    result = find_differences_and_product2(adapters)
-    assert result == (69, 33, 2277)
-    # print("timeit 2", timeit.repeat(partial(find_differences_and_product2, adapters)))
-    result = find_combinations(adapters)
-    assert result == 37024595836928
-    
+    seats = load_seats("data/day-11.txt")
+    result = find_stable_configuration(seats, 4, direct_neighbors, 0.25)
+    assert result == 2251
+    result = find_stable_configuration(seats, 5, line_of_sight, 0.25)
+    assert result == 2019
