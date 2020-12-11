4d020d26a3b261dfdb005c426e1686034f3fff4b
Sir Rob <git@netsak.de>
2020-12-11 09:48:37 +0100

    Remove benchmark

--- day-11.py
+++ day-11.py
@@ -18,8 +18,8 @@ def find_differences_and_product(adapters,  max_diff=3, build_in_joltage=3):
     diff_1 = sum(1 for d in differences if d == 1)
     diff_3 = sum(1 for d in differences if d == 3)
     product = diff_1 * diff_3
-    # print(f"There are {diff_1} differences of 1 jolt and {diff_3} differences of 3 jolt.", end=" ")
-    # print(f"The product joltage is {product}.")
+    print(f"There are {diff_1} differences of 1 jolt and {diff_3} differences of 3 jolt.", end=" ")
+    print(f"The product joltage is {product}.")
     return diff_1,diff_3, product
 
 
@@ -32,8 +32,8 @@ def find_differences_and_product2(adapters, build_in_joltage=3):
         differences[adapter-last] += 1
         last = adapter
     joltage_product = differences[1] * differences[3]
-    # print(f"There are {differences[1]} differences of 1 jolt and {differences[3]} differences of 3 jolt.", end=" ")
-    # print(f"The product joltage is {joltage_product}.")
+    print(f"There are {differences[1]} differences of 1 jolt and {differences[3]} differences of 3 jolt.", end=" ")
+    print(f"The product joltage is {joltage_product}.")
     return differences[1], differences[3], joltage_product
                 
 
@@ -53,10 +53,10 @@ if __name__ == "__main__":
     adapters = load_adapters("data/day-10-example1.txt")
     result = find_differences_and_product(adapters)
     assert result == (7, 5, 35)
-    print("timeit 1", timeit.repeat(partial(find_differences_and_product, adapters)))
+    # print("timeit 1", timeit.repeat(partial(find_differences_and_product, adapters)))
     result = find_differences_and_product2(adapters)
     assert result == (7, 5, 35)
-    print("timeit 2", timeit.repeat(partial(find_differences_and_product2, adapters)))
+    # print("timeit 2", timeit.repeat(partial(find_differences_and_product2, adapters)))
     result = find_combinations(adapters)
     assert result == 8
     adapters = load_adapters("data/day-10-example2.txt")
@@ -70,10 +70,10 @@ if __name__ == "__main__":
     adapters = load_adapters("data/day-10.txt")
     result = find_differences_and_product(adapters)
     assert result == (69, 33, 2277)
-    print("timeit 1", timeit.repeat(partial(find_differences_and_product, adapters)))
+    # print("timeit 1", timeit.repeat(partial(find_differences_and_product, adapters)))
     result = find_differences_and_product2(adapters)
     assert result == (69, 33, 2277)
-    print("timeit 2", timeit.repeat(partial(find_differences_and_product2, adapters)))
+    # print("timeit 2", timeit.repeat(partial(find_differences_and_product2, adapters)))
     result = find_combinations(adapters)
     assert result == 37024595836928
     
